a3dc66f9848fa4ad753b5c0cd4f04b3af98d6d58
Gustavo Gama <gustavogama494@gmail.com>
2023-05-14 09:41:25 -0300

    Rota de Criação de usuário desenvolvida

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,10 +1,54 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repository"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar("cadastro"); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+	defer db.Close()
+
+	repositorio := repository.NovoRepositoDeUsuario(db)
+	usuarioID, erro := repositorio.CriarUsuario(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	usuario, erro = repositorio.BuscarPorId(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
 }
 
 // BuscarUsuario busca todos os usuários salvos no banco
--- src/repository/usuarios.go
+++ src/repository/usuarios.go
@@ -15,10 +15,46 @@ func NovoRepositoDeUsuario(db *sqlx.DB) *Usuarios {
 	return &Usuarios{db}
 }
 
+func (repositorio Usuarios) CriarUsuario(Usuario models.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Exec(
+		` INSERT INTO Usuarios (nome, email, senha ) values (?,?,?) `,
+		Usuario.Nome,
+		Usuario.Email,
+		Usuario.Senha,
+	)
+	if erro != nil {
+		return 0, erro
+	}
+
+	linhasAfetadas, erro := statement.RowsAffected()
+	if erro != nil || linhasAfetadas == 0 {
+		return 0, erro
+	}
+
+	usuarioID, erro := statement.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+	return uint64(usuarioID), nil
+}
+
+// BuscarPorId busca um usuário pelo ID
+func (repositorio Usuarios) BuscarPorId(usuarioId uint64) (models.Usuario, error) {
+	usuarios := models.Usuario{}
+	erro := repositorio.db.Get(&usuarios,
+		` SELECT id,nome,email,criadoem FROM Usuarios WHERE id = ? `,
+		usuarioId,
+	)
+	if erro != nil {
+		return models.Usuario{}, erro
+	}
+	return usuarios, nil
+}
+
 // BuscarPorEmail busca um usuário por email e retorna seu id e senha com hash
 func (repositorio Usuarios) BuscarPorEmail(email string) (models.Usuario, error) {
 	usuarios := models.Usuario{}
-	erro := repositorio.db.Get(&usuarios, "SELECT id,senha FROM usuarios WHERE email = ?", email)
+	erro := repositorio.db.Get(&usuarios, "SELECT id,senha FROM Usuarios WHERE email = ?", email)
 	if erro != nil {
 		return models.Usuario{}, erro
 	}
