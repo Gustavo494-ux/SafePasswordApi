b4014e753250c8411cbf274e0676bc73455a72af
Gustavo Gama <gustavogama494@gmail.com>
2023-08-15 17:05:44 -0300

    Alterações docker

--- src/router/route.go
+++ src/router/route.go
@@ -3,7 +3,6 @@ package router
 import (
 	"net/http"
 	router "safePasswordApi/src/router/routes"
-	"safePasswordApi/src/routines"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -13,7 +12,7 @@ import (
 func Gerar() *echo.Echo {
 	e := echo.New()
 	e.GET("/health", func(c echo.Context) error {
-		c.Response().Before(routines.AnalyzingCode)
+		// c.Response().Before(routines.AnalyzingCode)
 		return c.String(http.StatusOK, time.Now().Format(time.RFC3339Nano))
 	})
 
