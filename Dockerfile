fe5f24a3b7822c782851d4ed12e796f2b73bcf56
Gustavo Gama <gustavogama494@gmail.com>
2023-09-07 18:09:13 -0300

    Implementação inicial do swagger.
    Foram definidas apenas as informações gerais no pacote main.

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,23 @@ FROM golang:1.20
 
 WORKDIR /usr/src/app/SafePasswordApi
 
+# ## install tools
+RUN go install github.com/swaggo/swag/cmd/swag@latest
+
+
 # pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
 COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 
-COPY . ./        
+COPY . ./      
+
+# Generate the api documentation
+RUN swag init
+
+# project build
 RUN go build -o safePassword 
 
-# EXPOSE ${API_PORT}
+EXPOSE ${API_PORT}
 
+# ## Run the application
 CMD ["./safePassword"]
--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	_ "safePasswordApi/docs"
 	"safePasswordApi/src/configs"
 	"safePasswordApi/src/router"
 	"time"
+
+	echoSwagger "github.com/swaggo/echo-swagger" // echo-swagger middleware
 )
 
 func init() {
 	configs.InitializeConfigurations()
 }
 
-func main() {
+// @title SafePasswordApi
+// @version 0.1
+// @description SafePasswordApi é uma API de gerenciamento de senhas com criptografia integral,
+// @description proporcionando armazenamento e acesso seguros para dados sensíveis de usuários,
+// @description por meio de métodos eficientes de manipulação de senhas.
 
+// @contact.name Gustavo Gama
+// @contact.url https://www.linkedin.com/in/gustavo-gama-966b341b8/
+// @contact.email gustavogama494@gmail.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+func main() {
 	r := router.Gerar()
 	r.Server.WriteTimeout = 30 * time.Second
+
+	r.GET("/swagger/*", echoSwagger.WrapHandler)
 	r.Logger.Fatal(r.Start(fmt.Sprintf(":%d", configs.Port)))
 }
