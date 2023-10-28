0f562805e7242a4f85fda286b1be8aaf29bc9da4
Gustavo Gama <gustavogama494@gmail.com>
2023-10-28 16:00:01 -0300

    implementação de logs na autenticacao_test

--- src/security/auth/autenticacao_test.go
+++ src/security/auth/autenticacao_test.go
@@ -1,12 +1,15 @@
 package auth_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"safePasswordApi/src/configs"
 	"safePasswordApi/src/security/auth"
 	"testing"
 	"time"
+
+	"safePasswordApi/src/modules/logger"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -15,12 +18,16 @@ import (
 func TestCriarTokenJWT(t *testing.T) {
 	tokenString, err := auth.CriarTokenJWT(uint64(1))
 	if err != nil {
+		logger.Logger().Error("Erro ao criar token JWT:", err)
 		t.Errorf("Erro ao criar token JWT: %v", err)
 	}
 
 	if len(tokenString) == 0 {
+		logger.Logger().Error("Token JWT vazio", err)
 		t.Error("Token JWT vazio")
 	}
+
+	logger.Logger().Info("Teste de CriarTokenJWT executado com sucesso!")
 }
 
 func TestValidarToken(t *testing.T) {
@@ -33,6 +40,7 @@ func TestValidarToken(t *testing.T) {
 
 	tokenString, err := token.SignedString([]byte(configs.SecretKeyJWT))
 	if err != nil {
+		logger.Logger().Error("Erro ao criar o token:", err)
 		t.Errorf("Erro ao criar o token: %v", err)
 	}
 
@@ -44,19 +52,22 @@ func TestValidarToken(t *testing.T) {
 	// Chama a função ValidarToken
 	err = auth.ValidarToken(c)
 	if err != nil {
+		logger.Logger().Error("Erro ao validar token:", err)
 		t.Errorf("Erro ao validar token: %v", err)
 	}
+
+	logger.Logger().Info("Teste de ValidarToken executado com sucesso!")
 }
 
 func TestExtrairUsuarioID(t *testing.T) {
 	usuarioID := uint64(1)
-
 	// Cria um token válido com o ID do usuário
 	tokenString, err := auth.CriarTokenJWT(usuarioID)
 	if err != nil {
+		logger.Logger().Error("Erro ao extrair ID do usuário:", err)
 		t.Errorf("Erro ao extrair ID do usuário: %v", err)
-
 	}
+
 	// Cria um contexto com o token no cabeçalho de autorização
 	req := httptest.NewRequest(http.MethodGet, "/test", nil)
 	req.Header.Set("Authorization", "Bearer "+tokenString)
@@ -66,35 +77,14 @@ func TestExtrairUsuarioID(t *testing.T) {
 	id, err := auth.ExtrairUsuarioID(c)
 
 	if err != nil {
+		logger.Logger().Error("Erro ao extrair ID do usuário:", err)
 		t.Errorf("Erro ao extrair ID do usuário: %v", err)
 	}
 
 	if id != usuarioID {
+		logger.Logger().Error(fmt.Sprintf("ID do usuário incorreto. Esperado: %d		btido: %d: ", usuarioID, id), err)
 		t.Errorf("ID do usuário incorreto. Esperado: %d, Obtido: %d", usuarioID, id)
 	}
+
+	logger.Logger().Info("Teste de ExtrairUsuarioID executado com sucesso!")
 }
-
-/*
-// CriarToken retorna um token assinado com as permissões do usuário
-func TestCriarTokenJWT(t *testing.T) {
-
-}
-
-// ValidarToken verifica se o token passado na requisição é válido
-func TestValidarToken(t *testing.T) {
-
-}
-
-// ExtrairUsuarioID retorna o usuarioId que está salvo no token
-func TestExtrairUsuarioID(t *testing.T) {
-
-}
-
-func TestextrairToken(c echo.Context) {
-
-}
-
-func TestRetornarChaveDeVerificacao(t *testing.T) {
-
-}
-*/
