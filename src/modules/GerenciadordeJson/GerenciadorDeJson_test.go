d7752b5cd0b1eb99ff515b6d53bb7c224cabcff5
Gustavo Gama <gustavogama494@gmail.com>
2023-10-28 15:22:33 -0300

    Implementação de logs no arquivo GerenciadorDeJson_test

--- src/modules/GerenciadordeJson/GerenciadorDeJson_test.go
+++ src/modules/GerenciadordeJson/GerenciadorDeJson_test.go
@@ -1,9 +1,21 @@
 package GerenciadordeJson_test
 
 import (
+	"fmt"
+	"os"
 	"safePasswordApi/src/modules/GerenciadordeJson"
+	"safePasswordApi/src/modules/logger"
+	"safePasswordApi/src/routines/inicializacao"
 	"testing"
 )
+
+// TestMain:Função executada antes das demais
+func TestMain(m *testing.M) {
+	inicializacao.CarregarDotEnv()
+	inicializacao.InicializarEncriptacao()
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
 
 func TestInterfaceParaJsonString(t *testing.T) {
 	type Pessoa struct {
@@ -14,12 +26,15 @@ func TestInterfaceParaJsonString(t *testing.T) {
 	jsonStr, err := GerenciadordeJson.InterfaceParaJsonString(p)
 	if err != nil {
 		t.Errorf("Erro ao converter interface{} para JSON: %v", err)
+		logger.Logger().Error("Erro ao converter interface{} para JSON", err)
 	}
 
 	expected := `{"nome":"João","idade":30}`
 	if jsonStr != expected {
 		t.Errorf("JSON esperado: %s, JSON retornado: %s", expected, jsonStr)
+		logger.Logger().Error(fmt.Sprintf("JSON esperado: %s, JSON retornado: %s", expected, jsonStr), err)
 	}
+	logger.Logger().Info("Teste TestInterfaceParaJsonString executado com sucesso!")
 }
 
 func TestJsonStringParaInterface(t *testing.T) {
@@ -31,10 +46,13 @@ func TestJsonStringParaInterface(t *testing.T) {
 	jsonData, err := GerenciadordeJson.JsonStringParaInterface(jsonStr)
 	if err != nil {
 		t.Errorf("Erro ao converter JSON para interface{}: %v", err)
+		logger.Logger().Error("Erro ao converter JSON para interface{}", err)
 	}
 	if !compareMaps(jsonData.(map[string]interface{}), expected) {
 		t.Errorf("Interface{} esperada: %v, Interface{} retornada: %v", expected, jsonData)
+		logger.Logger().Error(fmt.Sprintf("Interface{} esperada: %v, Interface{} retornada: %v", expected, jsonData), err)
 	}
+	logger.Logger().Info("Teste TestJsonStringParaInterface executado com sucesso!")
 }
 
 func compareMaps(m1, m2 map[string]interface{}) bool {
