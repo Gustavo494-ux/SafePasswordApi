acd0747285913edeb4afebf6822820d2c66914ba
Gustavo Gama <gustavogama494@gmail.com>
2023-10-30 10:55:25 -0300

    melhoria das respostas

--- src/models/resposta.go
+++ src/models/resposta.go
@@ -10,9 +10,9 @@ import (
 type Resposta struct {
 	StatusCode            int
 	Mensagem              string
-	Error                 error
 	DadosAdicionaisLogger []interface{}
 	Contexto              echo.Context
+	dadoRetorno           interface{}
 }
 
 // RespostaRequisicao cria uma instância resposta
@@ -20,20 +20,32 @@ func RespostaRequisicao(c echo.Context) *Resposta {
 	return &Resposta{Contexto: c}
 }
 
-// Erro: configura a resposta para um erro
-func (resposta *Resposta) Erro(StatusCode int, erro error, MensagemLogger string, DadosAdicionaisLogger ...interface{}) *Resposta {
+// Erro: configura a resposta para resposta para uma requisição bem sucedida
+func (resposta *Resposta) Sucesso(StatusCode int, DadosResposta interface{}, MensagemLogger string, DadosAdicionaisLogger ...interface{}) *Resposta {
 	resposta.StatusCode = StatusCode
-	resposta.Error = erro
-	resposta.Mensagem = fmt.Sprintf("%s Id Requisição: %s", MensagemLogger, resposta.Contexto)
+	resposta.dadoRetorno = DadosResposta
+	resposta.Mensagem = fmt.Sprintf("%s , Id Requisição: %s, StatusCode: %d", MensagemLogger, resposta.Contexto, StatusCode)
 	resposta.DadosAdicionaisLogger = DadosAdicionaisLogger
-	logger.Logger().Error(resposta.Mensagem, resposta.Error, resposta.DadosAdicionaisLogger...)
+	logger.Logger().Info(resposta.Mensagem, resposta.DadosAdicionaisLogger...)
 	return resposta
 }
 
-func (resposta *Resposta) JSON() error {
-	return resposta.Contexto.JSON(resposta.StatusCode, resposta.Error.Error())
+// Erro: configura a resposta para um erro
+func (resposta *Resposta) Erro(StatusCode int, erro error, MensagemLogger string, DadosAdicionaisLogger ...interface{}) *Resposta {
+	resposta.StatusCode = StatusCode
+	resposta.dadoRetorno = erro
+	resposta.Mensagem = fmt.Sprintf("%s , Id Requisição: %s, StatusCode: %d", MensagemLogger, resposta.Contexto, StatusCode)
+	resposta.DadosAdicionaisLogger = DadosAdicionaisLogger
+	logger.Logger().Error(resposta.Mensagem, erro, resposta.DadosAdicionaisLogger...)
+	return resposta
 }
 
+// JSON: envia uma resposta para a requisição em formato JSON
+func (resposta *Resposta) JSON() error {
+	return resposta.Contexto.JSON(resposta.StatusCode, resposta.dadoRetorno)
+}
+
+// String: envia uma resposta para a requisição em formato String
 func (resposta *Resposta) String() error {
-	return resposta.Contexto.JSON(resposta.StatusCode, resposta.Error.Error())
+	return resposta.Contexto.JSON(resposta.StatusCode, resposta.dadoRetorno)
 }
